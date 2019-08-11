6f404ca76a62b021c53171fe9d401c3ddee980ad
aaronespasa <aaespasa@gmail.com>
2019-08-12 00:00:53 +0200

    Adding environment variables

--- public/index.php
+++ public/index.php
@@ -10,6 +10,10 @@ require_once '../vendor/autoload.php';
 
 session_start();
 
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
+$dotenv->load();
+
+
 use Aura\Router\RouterContainer;
 use Zend\Diactoros\Response\RedirectResponse;
 
@@ -20,10 +24,10 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cursophp',
-    'username'  => 'root',
-    'password'  => '',
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
